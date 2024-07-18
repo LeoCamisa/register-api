254251623bc98e04098ab4c4c86381338e5853b0
Léo Camisa Echeverria <tpleosuporte@gmail.com>
2024-07-18 18:56:36 -0300

    :sparkles: Added RegisteredTimeModule for managing registered times
    - Configured TypeOrmModule to include RegisteredTime entity
    - Imported UserModule to access user-related functionality
    - Provided RegisteredTimeService and RegisteredTimeResolver
    - Exported RegisteredTimeService for use in other modules

--- src/registered-time/registered-time.module.ts
+++ src/registered-time/registered-time.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RegisteredTime } from './registered-time.entity/registered-time.entity';
 import { RegisteredTimeService } from './registered-time.service';
 import { RegisteredTimeResolver } from './registered-time.resolver';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  providers: [RegisteredTimeService, RegisteredTimeResolver]
+  imports: [TypeOrmModule.forFeature([RegisteredTime]),
+  UserModule,
+],
+  providers: [RegisteredTimeService, RegisteredTimeResolver],
+  exports: [RegisteredTimeService],
 })
 export class RegisteredTimeModule {}
