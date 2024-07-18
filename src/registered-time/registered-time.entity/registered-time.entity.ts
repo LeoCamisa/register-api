5d224af600805565d0bbc1bba85e7117c741b073
Léo Camisa Echeverria <tpleosuporte@gmail.com>
2024-07-18 17:26:30 -0300

    :memo: rename folder

--- src/registered-time/registered-time.entity/registered-time.entity.ts
+++ src/registered-time/registered-time.entity/registered-time.entity.ts
@@ -1,6 +1,24 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn} from 'typeorm';
 import { User } from 'src/user/user.entity/user.entity';
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+
+@ObjectType()
+@Entity()
+export class RegisteredTime {
+  @Field(() => Int)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field()
+  @Column({ nullable: false })
+  time_registered: string;
+
+  @Field(() => Int)
+  @Column({ nullable: false })
+  userId: number;
+
   @Field(() => User)
   @ManyToOne(() => User, (user) => user.registeredTimes, { nullable: false, onDelete: 'CASCADE' })
   @JoinColumn({ name: 'userId' })
   user: User;
+}
