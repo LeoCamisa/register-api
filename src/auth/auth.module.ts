bca4c092f9309c0991b71aeac0eba31a5be24fe4
Léo Camisa Echeverria <tpleosuporte@gmail.com>
2024-07-18 18:46:37 -0300

    :sparkles: Added AuthModule for authentication management
    - Configured JwtModule with environment variables
    - Imported UserModule and PassportModule
    - Provided AuthService, AuthResolver, and JwtStrategy
    - Set up AuthGuard for GraphQL authentication"

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModule } from '../user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  providers: [AuthService, AuthResolver]
+  imports: [
+    ConfigModule.forRoot(),
+    UserModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '60m' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [AuthService, AuthResolver, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
