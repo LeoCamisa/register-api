1740fb3e2926bb1b0ce7a78b2821fc64a7c00320
Léo Camisa Echeverria <tpleosuporte@gmail.com>
2024-07-18 16:16:28 -0300

    :sparkles:  Create initial schema for db :sparkles:
    Add init.sql to create 'user' and 'registered_time' tables

---
+++ init.sql
@@ -0,0 +1,18 @@
+-- Create table user
+CREATE TABLE IF NOT EXISTS "user" (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL
+),
+
+-- Create table registered_time
+CREATE TABLE IF NOT EXISTS "registered_time" (
+    id SERIAL PRIMARY KEY,
+    time_registered TIMESTAMP NOT NULL,
+    userId INTEGER NOT NULL,
+    CONSTRAINT fk_user
+       FOREIGN KEY(userId)
+       REFERENCES "user"(id)
+);
