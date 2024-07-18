f553ee64c9bb83cf8200733510fcfd9ec169c1f2
Léo Camisa Echeverria <tpleosuporte@gmail.com>
2024-07-18 17:15:00 -0300

    :memo: add config .env in docker-compose and app.module to typeORM :whale2:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { RegisteredTimeModule } from './registered-time/registered-time.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT, 10),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      migrations: [__dirname + '/migration/*{.ts,.js}'],
+      synchronize: true,
+    }),
+    AuthModule, RegisteredTimeModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
