9e8d02b140226bd9fe82724b837a4db498e80e1b
Léo Camisa Echeverria <tpleosuporte@gmail.com>
2024-07-18 17:49:55 -0300

    :sparkles: Added user service and resolver for GraphQL :sparkles:

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,4 +1,24 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { UserService } from './user.service';
+import { CreateUserInput } from './dto/create-user.input';
+import { UserType } from './dto/user.type';
 
-@Resolver()
-export class UserResolver {}
+@Resolver(() => UserType)
+export class UserResolver {
+  constructor(private userService: UserService) {}
+
+  @Query(() => [UserType])
+  async users(): Promise<UserType[]> {
+    return this.userService.findAll();
+  }
+
+  @Query(() => UserType, { name: 'user' })
+  async getUser(@Args('id', { type: () => Number }) id: number): Promise<UserType> {
+    return this.userService.findById(id);
+  }
+
+  @Mutation(() => UserType)
+  async createUser(@Args('input') input: CreateUserInput): Promise<UserType> {
+    return this.userService.createUser(input);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity/user.entity';
+import { CreateUserInput } from './dto/create-user.input';
+import { UserType } from './dto/user.type';
+
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async findByEmail(email: string): Promise<User | undefined> {
+    return this.userRepository.findOne({ where: { email } });
+  }
+
+  async findById(id: number): Promise<User | undefined> {
+    return this.userRepository.findOne({ where: { id } });
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  async create(name: string): Promise<User> {
+    const newUser = this.userRepository.create({ name });
+    return this.userRepository.save(newUser);
+  }
+
+  async createUser(input: CreateUserInput): Promise<UserType> {
+    console.log('Received input:', input);
+    try {
+      const user = this.userRepository.create({ ...input});
+      await this.userRepository.save(user);
+      return user;
+    } catch (error) {
+      console.error('Error creating user:', error);
+      throw error;
+    }
+  }
+}
